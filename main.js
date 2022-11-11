b6ceb2527a14a9f71a98c017e1607e11f61da604
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-11-11 20:39:29 +0900

    v1

---
+++ main.js
@@ -0,0 +1,19 @@
+let video = document.getElementsByClassName("video-stream")[0];
+if (video != null) {
+    video.classList.remove("html5-main-video");
+}
+
+// let videoFrame = document.getElementsByClassName("ytp-iv-video-content")[0];
+// if (videoFrame != null) {
+//     let img_elm = document.createElement("img");
+//     img_elm.src = "https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png";
+//     img_elm.width = 300;
+//     img_elm.height = 300;
+//     videoFrame.appendChild(img_elm);
+// }
+
+// window.addEventListener('resize', function () {
+//     console.log(element.style.width);
+//     //    element.style.setProperty('width', '100px', 'important');
+// });
+
