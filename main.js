5b8222ebf97d5acc60a858be19fefab1dc603f70
i-icc <ichi623110@gmail.com>
2022-11-18 00:03:17 +0900

    とりあえずかんせい

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ if (video != null) {
     video.classList.remove("html5-main-video");
 }
 
-let videoFrame = document.getElementById("movie_player");
+// let videoFrame = document.getElementById("movie_player");
+let videoFrame = document.getElementsByClassName("html5-video-container")[0];
 if (videoFrame != null) {
     let img_elm = document.createElement("img");
     img_elm.src = "https://user-images.githubusercontent.com/52018660/202184627-e3718e58-26ec-4d25-b271-a1d7955ad50b.png";
